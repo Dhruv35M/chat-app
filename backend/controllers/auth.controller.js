b862875fb13ff12f6947c11ed269252700c7ebc0
dhruv mishra <dhruv35m@gmail.com>
2024-05-28 05:14:02 +0530

    add jwt and cookie

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signupUser = async (req, res) => {
   try {
@@ -30,6 +31,7 @@ export const signupUser = async (req, res) => {
     });
 
     if (newUser) {
+      await generateTokenAndSetCookie(newUser._id, res);
       await newUser.save();
 
       res.status(201).json({
