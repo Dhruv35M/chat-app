e4fad7d688a60d457419eaebeca8e3041daa539e
dhruv mishra <dhruv35m@gmail.com>
2024-05-28 04:28:12 +0530

    add user model

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import auth from "./routes/auth.routes.js";
 import connectToDB from "./db/connection.js";
-dotenv.config();
 
 const app = express();
-
 const PORT = process.env.PORT || 5000;
 
+dotenv.config();
+app.use(express.json());
 app.use("/api/auth", auth);
 
 app.listen(PORT, () => {
