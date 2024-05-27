9cda9a9715597a4a1bf51e0e61c54db344c06de0
dhruv mishra <dhruv35m@gmail.com>
2024-05-28 04:47:57 +0530

    user signup

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,45 @@
+import User from "../models/user.model.js";
+
 export const signupUser = async (req, res) => {
-  res.send("signup");
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Passwords don't match" });
+    }
+
+    const user = await User.findOne({ username });
+
+    if (user) {
+      return res.status(400).json({ error: "Username alrady exists" });
+    }
+
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    const newUser = new User({
+      fullName,
+      username,
+      password,
+      gender,
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({
+      _id: newUser._id,
+      fullName: newUser.fullName,
+      username: newUser.username,
+      gender: newUser.gender,
+      profilePic: newUser.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({
+      error: "Internal Server Error",
+    });
+  }
 };
 
 export const loginUser = async (req, res) => {
@@ -9,3 +49,5 @@ export const loginUser = async (req, res) => {
 export const logoutUser = async (req, res) => {
   res.send("logout");
 };
+
+// RANDOM PROFILE PIC API: https://avatar-placeholder.iran.liara.run/
