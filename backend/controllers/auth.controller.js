7ed6dbe2a4425bded74d4244bd8db3c1f7440513
dhruv mishra <dhruv35m@gmail.com>
2024-05-28 05:31:38 +0530

    logout user

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -80,7 +80,13 @@ export const loginUser = async (req, res) => {
 };
 
 export const logoutUser = async (req, res) => {
-  res.send("logout");
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 // RANDOM PROFILE PIC API: https://avatar-placeholder.iran.liara.run/
